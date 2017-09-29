06beac9e50c010f7b380dbb0ed1fcedcdd098977
jiyinyiyong <jiyinyiyong@gmail.com>
2017-09-29 15:55:54 +0800

    simplify render.cljs

--- src/app/render.cljs
+++ src/app/render.cljs
@@ -24,15 +24,18 @@
   (let [html-content (make-string (comp-container schema/store))
         manifest (.parse js/JSON (slurp "dist/assets-manifest.json"))
         cljs-manifest (.parse js/JSON (slurp "dist/manifest.json"))
-        cdn (if previews? "" "http://repo-cdn.b0.upaiyun.com/coworkflow/")]
+        cdn (if previews? "" "http://repo-cdn.b0.upaiyun.com/coworkflow/")
+        prefix-cdn (fn [x] (str cdn x))]
     (make-page
      html-content
      (merge
       base-info
-      {:styles [(str cdn (aget manifest "main.css"))],
-       :scripts [(str cdn (aget manifest "main.js"))
-                 (str cdn (-> cljs-manifest (aget 0) (aget "js-name")))
-                 (str cdn (-> cljs-manifest (aget 1) (aget "js-name")))],
+      {:styles [(prefix-cdn (aget manifest "main.css"))],
+       :scripts (map
+                 prefix-cdn
+                 [(aget manifest "main.js")
+                  (-> cljs-manifest (aget 0) (aget "js-name"))
+                  (-> cljs-manifest (aget 1) (aget "js-name"))]),
        :ssr "respo-ssr"}))))
 
 (defn main! []
