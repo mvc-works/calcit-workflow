6a5142abd72632daa68344b00474af6f6b55d317
ChenYong <jiyinyiyong@dingtalk.com>
2017-12-29 18:18:47 +0800

    clean unused code

--- src/build/upload.clj
+++
@@ -1,7 +0,0 @@
-
-(ns build.upload
-  (:require [clojure.java.shell :refer [sh]]))
-
-(defn -main []
-  (println (sh "rsync" "-avr --progress dist/* tiye.me:cdn/coworkflow"))
-  (println (sh "rsync" "-avr --progress dist/{index.html,manifest.json} tiye.me:repo/mvc-works/coworkflow/")))
--- src/build/watch.clj
+++
@@ -1,9 +0,0 @@
-
-(ns build.watch
-  (:require [shadow.cljs.devtools.api :as api]
-            [shadow.cljs.devtools.server :as server]
-            [clojure.java.shell :refer [sh]]))
-
-(defn -main []
-  (api/watch :browser)
-  (api/repl :browser))
