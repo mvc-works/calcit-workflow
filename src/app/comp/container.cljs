c5b466f3a8b4faf7b53a6a58ff9a87dfda325572
ChenYong <jiyinyiyong@dingtalk.com>
2018-01-12 12:36:25 +0800

    switch to new Respo event syntax

--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -2,7 +2,9 @@
 (ns app.comp.container
   (:require [hsl.core :refer [hsl]]
             [respo-ui.style :as ui]
-            [respo.macros :refer [defcomp cursor-> <> div button textarea span]]
+            [respo.macros
+             :refer
+             [defcomp cursor-> action-> mutation-> <> div button textarea span]]
             [verbosely.core :refer [verbosely!]]
             [respo.comp.space :refer [=<]]
             [reel.comp.reel :refer [comp-reel]]))
@@ -17,12 +19,12 @@
      {:value (:content store),
       :placeholder "Content",
       :style (merge ui/textarea {:width 640, :height 320}),
-      :on {:input (fn [e d! m!] (d! :content (:value e)))}})
+      :on-input (action-> :content (:value %e))})
     (=< "8px" nil)
     (div
      {}
      (button
       {:style ui/button,
        :inner-text (str "run"),
-       :on {:click (fn [e d! m!] (println (:content store)))}}))
+       :on-click (fn [e d! m!] (println (:content store)))}))
     (cursor-> :reel comp-reel states reel {}))))
