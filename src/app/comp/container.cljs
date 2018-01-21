5834fab2346a841ecc37df6f1d145d8b238ac850
ChenYong <jiyinyiyong@dingtalk.com>
2018-01-21 17:32:20 +0800

    upgrade deps

--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -1,7 +1,7 @@
 
 (ns app.comp.container
   (:require [hsl.core :refer [hsl]]
-            [respo-ui.style :as ui]
+            [respo-ui.core :as ui]
             [respo.macros
              :refer
              [defcomp cursor-> action-> mutation-> <> div button textarea span]]
