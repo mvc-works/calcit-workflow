2cd17d74e349bf1c4f6e4efa22af4b1ed0976eed
jiyinyiyong <jiyinyiyong@gmail.com>
2018-02-25 00:27:55 +0800

    upgrade shadow-cljs

--- src/app/updater.cljs
+++ src/app/updater.cljs
@@ -1,7 +1,7 @@
 
 (ns app.updater (:require [respo.cursor :refer [mutate]]))
 
-(defn updater [store op op-data]
+(defn updater [store op op-data op-id op-time]
   (case op
     :states (update store :states (mutate op-data))
     :content (assoc store :content op-data)
