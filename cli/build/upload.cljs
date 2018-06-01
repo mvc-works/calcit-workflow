28eb3d0f726d7f5c9b3fa84b75e84b7c191d6c4d
jiyinyiyong <jiyinyiyong@gmail.com>
2018-06-02 00:04:25 +0800

    moving more info to config.cljs

--- cli/build/upload.cljs
+++ cli/build/upload.cljs
@@ -1,18 +1,13 @@
 
 (ns build.upload
-  (:require ["child_process" :as child-process]))
-
-(def configs {:orgization "mvc-works"
-              :name "calcit-workflow"
-              :cdn "calcit-workflow"})
+  (:require ["child_process" :as child-process]
+            [app.config :as config]))
 
 (defn sh! [command]
   (println command)
   (println (.toString (child-process/execSync command))))
 
 (defn -main []
-  (sh! (str "rsync -avr --progress dist/* tiye.me:cdn/" (:cdn configs)))
-  (sh!
-    (str "rsync -avr --progress dist/{index.html,manifest.json} tiye.me:repo/"
-      (:orgization configs) "/"
-      (:name configs) "/")))
+  (sh! (str "rsync -avr --progress dist/* tiye.me:cdn/" (:cdn-path config/site)))
+  (sh! (str "rsync -avr --progress dist/{index.html,manifest.json} tiye.me:repo/"
+            (:upload-path config/site) "/")))
--- src/app/config.cljs
+++ src/app/config.cljs
@@ -7,4 +7,8 @@
   {:storage "workflow",
    :dev-ui "http://localhost:8100/main.css",
    :release-ui "http://cdn.tiye.me/favored-fonts/main.css",
-   :cdn "http://cdn.tiye.me/calcit-workflow/"})
+   :cdn "http://cdn.tiye.me/calcit-workflow/",
+   :cdn-path "calcit-workflow",
+   :title "Calcit",
+   :icon "http://cdn.tiye.me/logo/mvc-works.png",
+   :upload-path "mvc-works/calcit-workflow"})
