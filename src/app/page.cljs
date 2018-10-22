441a0c220b7d43695e3b3985ca29a3654efbdc18
ChenYong <chenyong@jimeng.io>
2018-10-22 17:11:45 +0800

    simplify string interpolation syntax

--- src/app/page.cljs
+++ src/app/page.cljs
@@ -19,7 +19,7 @@
    ""
    (merge
     base-info
-    {:styles [(<< "http://~{(get-ip!)}:8100/main.css") "/entry/main.css"],
+    {:styles [(<< "http://~(get-ip!):8100/main.css") "/entry/main.css"],
      :scripts ["/client.js"],
      :inline-styles []})))
 
