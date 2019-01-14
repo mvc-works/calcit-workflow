7825d2ab9edb8ab02adb775525415d6c28cff72d
ChenYong <chenyong@jimeng.io>
2019-01-14 12:03:08 +0800

    only print logs during dev

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -15,7 +15,7 @@
   (atom (-> reel-schema/reel (assoc :base schema/store) (assoc :store schema/store))))
 
 (defn dispatch! [op op-data]
-  (comment println "Dispatch:" op)
+  (when config/dev? (println "Dispatch:" op))
   (reset! *reel (reel-updater updater @*reel op op-data)))
 
 (def mount-target (.querySelector js/document ".app"))
