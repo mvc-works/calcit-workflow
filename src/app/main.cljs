4fb058967787296eee8e372907e004c0d18349e6
jiyinyiyong <jiyinyiyong@gmail.com>
2019-03-18 01:06:26 +0800

    removing app.snippet

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -44,3 +44,5 @@
   (clear-cache!)
   (reset! *reel (refresh-reel @*reel schema/store updater))
   (println "Code updated."))
+
+(defn snippets [] (println config/cdn?))
--- src/app/snippet.cljs
+++
@@ -1,9 +0,0 @@
-
-(ns app.snippet )
-
-(defn main! []
-  (require '[clojure.string :as string])
-  (println (string/join "&&" (range 1000)))
-  (.clear js/console)
-  (defn f [x y] (* x y))
-  (println (f 2 4)))
